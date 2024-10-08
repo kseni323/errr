658186818db5985424c44018c958191d856afb59
Max <ytmolniy@gmail.com>
2024-10-08 22:20:24 +0300

    Добавление описания канала

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Новые Видео - YouTube Канал</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/style2.css">
+    <title>YouTube Молния official</title>
+    <!-- Подключение CSS файла -->
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/linksmenu.css">
+    <link href="dist/hamburgers.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
+
  <!-- Подключаем header -->
  @include('partials.header')
+
 
     <section id="videos">
         <div class="container">

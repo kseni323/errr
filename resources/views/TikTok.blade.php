6cdb3fc798f1c9f51af51f98ed4784fe6f1c0dfb
mask <maksimkharitonov28@yandex.ru>
2024-10-11 01:48:30 +0300

    Популярные идео TikTok

--- resources/views/TikTok.blade.php
+++ resources/views/TikTok.blade.php
@@ -35,7 +35,7 @@
                 <!-- Вставка видео через iframe -->
                 <div class="video-container1">
                 <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@molniyoff/video/7424208327350947079" data-video-id="7424208327350947079" style="max-width: 605px;min-width: 325px;"><section>
-        <a target="_blank" title="@molniyoff" href="https://www.tiktok.com/@molniyoff?refer=embed">@molniyoff</a> КОГДА МЯЧ ХОРОШО ОТСКОЧИЛ ОТ СТЕНЫ В ROCKET LEAGUE #rocketleague #rocketleaguefunny #rocketleagueclipss #rocketleaguevideo #rocketleaguevideos #recomendation #rocketleaguegoals #rocketleagueclips #rocketleague_community
+                <a target="_blank" title="@molniyoff" href="https://www.tiktok.com/@molniyoff?refer=embed">@molniyoff</a> КОГДА МЯЧ ХОРОШО ОТСКОЧИЛ ОТ СТЕНЫ В ROCKET LEAGUE #rocketleague #rocketleaguefunny #rocketleagueclipss #rocketleaguevideo #rocketleaguevideos #recomendation #rocketleaguegoals #rocketleagueclips #rocketleague_community
         <a title="рекомендации" target="_blank" href="https://www.tiktok.com/tag/%D1%80%D0%B5%D0%BA%D0%BE%D0%BC%D0%B5%D0%BD%D0%B4%D0%B0%D1%86%D0%B8%D0%B8?refer=embed">#рекомендации</a>
         <a title="recomendatios" target="_blank" href="https://www.tiktok.com/tag/recomendatios?refer=embed">#recomendatios</a>
         <a target="_blank" title="♬ оригинальный звук  - molniyoff" href="https://www.tiktok.com/music/оригинальный-звук-molniyoff-7424208354127465233?refer=embed">♬ оригинальный звук  - molniyoff</a>
@@ -51,23 +51,42 @@
             <h2>Популярные видео</h2>
             <div class="videos-grid">
                 <div class="video-item"> 
-
+                <blockquote width="300" height="500" class="tiktok-embed" cite="https://www.tiktok.com/@molniyoff/video/7420137682807934226" data-video-id="7420137682807934226" style="max-width: 605px;min-width: 325px;">
+                <section>
+                    <a target="_blank" title="@molniyoff" href="https://www.tiktok.com/@molniyoff?refer=embed">@molniyoff</a>
+                    #rocketleague #rocketleaguefunny #rocketleagueclipss #rocketleaguevideo
+                    <a title="recomendatios" target="_blank" href="https://www.tiktok.com/tag/recomendatios?refer=embed">#recomendatios</a>
+                    <a target="_blank" title="♬ оригинальный звук - molniyoff" href="https://www.tiktok.com/music/оригинальный-звук-molniyoff-7420137732053273345?refer=embed">♬ оригинальный звук - molniyoff</a>
+                </section>
+            </blockquote>
                 </div>
                 <div class="video-item"> 
-              
+                <blockquote width="300" height="500" class="tiktok-embed" cite="https://www.tiktok.com/@molniyoff/video/7416025706649390343" data-video-id="7416025706649390343" style="max-width: 605px;min-width: 325px;">
+                <section>
+                    <a target="_blank" title="@molniyoff" href="https://www.tiktok.com/@molniyoff?refer=embed">@molniyoff</a>
+                    Как Правильно Нужно Пришвартовываться К Причалу На Black Russia
+                    <a title="blackrussia" target="_blank" href="https://www.tiktok.com/tag/blackrussia?refer=embed">#blackrussia</a>
+                    <a target="_blank" title="♬ оригинальный звук - molniyoff" href="https://www.tiktok.com/music/оригинальный-звук-molniyoff-7416025747644926721?refer=embed">♬ оригинальный звук - molniyoff</a>
+                </section>
+            </blockquote>
                 </div>
                 <div class="video-item"> 
-         
+                <blockquote width="300" height="500" class="tiktok-embed" cite="https://www.tiktok.com/@molniyoff/video/7415157046405827858" data-video-id="7415157046405827858" style="max-width: 605px;min-width: 325px;">
+                <section>
+                    <a target="_blank" title="@molniyoff" href="https://www.tiktok.com/@molniyoff?refer=embed">@molniyoff</a>
+                    ЗАБИЛ ГОЛ С ОСТРОГО УГЛА В ROCKET LEAGUE #rocketleague#rocketleaguefunny #rocketleagueclipss #rocketleaguevideo #rocketleaguevideos #recomendation
+                    <a title="rocketleaguegoals" target="_blank" href="https://www.tiktok.com/tag/rocketleaguegoals?refer=embed">#rocketleaguegoals</a>
+                    <a target="_blank" title="♬ оригинальный звук - molniyoff" href="https://www.tiktok.com/music/оригинальный-звук-molniyoff-7415157087258282753?refer=embed">♬ оригинальный звук - molniyoff</a>
+                </section>
+            </blockquote>
                 </div>
-                <div class="video-item"> 
                
-                </div>
             </div>
         </section>
     </div>
 
     <section id="videos">
-        <div class="container">
+        <div class="container1">
         <h1>Новые видео</h1>
         <div class="video-grid">
             <!-- Видео 1 -->

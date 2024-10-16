21210861d404b5f23354f9c6e57dcaed6c1e04a5
mask <maksimkharitonov28@yandex.ru>
2024-10-16 21:04:18 +0300

    Добавлены стили для rutube

--- resources/views/Rutube.blade.php
+++ resources/views/Rutube.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/linksmenu.css">
+    <link rel="stylesheet" href="css/rutube.css">
     <link href="dist/hamburgers.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
@@ -19,56 +20,55 @@
  <!-- Подключаем header -->
  @include('partials.header')
 
-
-        <div class="containerin">
+        <div class="containerrutube">
              <!-- Подключаем header -->
  @include('partials.menu')
- <section id="videos">
-        <div class="container">
-             <!-- Подключаем menu -->
- @include('partials.menu')
+ <section id="videosrutube">
+    <div class="containerrutube">
+        @include('partials.menu')
         <h1>Новые видео</h1>
-        <div class="video-grid">
+        <div class="video-gridrut">
             <!-- Видео 1 -->
-            <div class="video-item">
-                <div class="video-placeholder">
-                <iframe width="720" height="405" src="https://rutube.ru/play/embed/cc12dd4eda5677f89edc13e0a5578e83/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+            <div class="video-itemrutube">
+                <div class="video-placeholderrutube">
+                    <iframe width="300" height="150" src="https://rutube.ru/play/embed/cc12dd4eda5677f89edc13e0a5578e83/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                    <h3>Как Объяснить Эту Доску С Препятствиями Другу В We Were Here Too #2</h3>
+                </div>
             </div>
-            <div><h3>Как Объяснить Эту Доску С Препятствиями Другу В We Were Here Too #2</h3></div>
 
             <!-- Видео 2 -->
-            <div class="video-item">
-                <div class="video-placeholder">
-                <iframe width="720" height="405" src="https://rutube.ru/play/embed/e7d1a445a0205bb549180eb4ea92a8b6/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+            <div class="video-itemrutube">
+                <div class="video-placeholderrutube">
+                    <iframe width="300" height="150" src="https://rutube.ru/play/embed/e7d1a445a0205bb549180eb4ea92a8b6/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                    <h3>КУПИЛ ЕЩЕ ОДИН СТЕЛЛАЖ ДЛЯ ТОВАРОВ В SUPERMARKET SIMULATOR</h3>
                 </div>
-                <h3>КУПИЛ ЕЩЕ ОДИН СТЕЛЛАЖ ДЛЯ ТОВАРОВ В SUPERMARKET SIMULATOR</h3>
             </div>
 
             <!-- Видео 3 -->
-            <div class="video-item">
-                <div class="video-placeholder">
-                <iframe width="720" height="405" src="https://rutube.ru/play/embed/2c29b57b3643e50ed7aeffed3a80a639/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-                <h3>Сыграл На BackFire Все Рейтинговые Режимы В Rocket League</h3>
+            <div class="video-itemrutube">
+                <div class="video-placeholderrutube">
+                    <iframe width="300" height="150" src="https://rutube.ru/play/embed/2c29b57b3643e50ed7aeffed3a80a639/" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                    <h3>Сыграл На BackFire Все Рейтинговые Режимы В Rocket League</h3>
+                </div>
             </div>
-        <!-- Блок популярных видео -->
-        <h2>Плейлисты</h2>
+        </div>
+    </div>
+</section>
+
+    <div><h2>Плейлисты</h2>
             <ul class="playlist-list">
             <span class="txt"><li><a href="https://rutube.ru/plst/552488/">We Were Here</a></li></span>
             <span class="txt"><li><a href="https://rutube.ru/plst/427852/">Rocket League</a></li></span>
             <span class="txt"><li><a href="https://rutube.ru/plst/429416/">Supermarket Simulator</a></li></span>
                 <!-- Добавление других плейлистов -->
             </ul>
+            </div>
         </div>
 
-    </div>
-
-     <!-- Подключаем footer -->
- @include('partials.footer')
-
+    <!-- Подключаем footer -->
+    @include('partials.footer')
 <!-- JavaScript для выпадающего меню -->
  <script type="text/javascript" src="js/menu.js"></script>
-     
-    
 </body>
 </html>
 </body>

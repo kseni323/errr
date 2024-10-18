a16a74526a53ddde9906682496d41297fe5b042b
mask <maksimkharitonov28@yandex.ru>
2024-10-18 03:35:13 +0300

    Форма идей подключение к бд

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\Idea;
 use Illuminate\Support\Facades\Mail;
 class IdeaController extends Controller
 {
@@ -12,13 +13,18 @@ class IdeaController extends Controller
             'email' => 'required|email|max:255',
             'idea' => 'required|string|max:1000',
         ]);
+        // Сохранение идеи в базу данных
+        $idea = new Idea();
+        $idea->name = $validated['name'];
+        $idea->email = $validated['email'];
+        $idea->idea = $validated['idea'];
+        $idea->save();
         // Отправка письма
         $to = "maksimkharitonov28@yandex.ru";
         $subject = "Предложение идеи от " . $validated['name'];
         $message = "Имя: " . $validated['name'] . "\nПочта: " . $validated['email'] . "\nИдея: " . $validated['idea'];
         mail($to, $subject, $message, "From: " . $validated['email']);
         // Перенаправляем обратно на ту же страницу с сообщением об успехе
-        return redirect()->route('sending.idea')->with('success', 'Ваша идея отправлена!');
-
+        return redirect()->back()->with('success', 'Ваша идея отправлена и сохранена в базу данных!');
     }
 }

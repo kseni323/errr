388d5431d1153df67e5db23d89a1e89c17113dfc
maks <maksimkharitonov28@yandex.ru>
2024-10-25 04:52:10 +0300

    Админ панель в2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,11 +62,12 @@ class RegisterController extends Controller
      * @return \App\Models\User
      */
     protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-    }
+{
+    return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+        'role_id' => 1, // Назначаем роль по умолчанию (например, роль "user")
+    ]);
+}
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id')->default(1); // Значение по умолчанию для роли "user"
             $table->rememberToken();
             $table->timestamps();
         });

264cbafff4a99ff64b65d002e9f5beccb63ce2ad
maks <ksenialukianova2000@gmail.com>
2024-10-31 17:49:33 +0300

    first commit

--- database/migrations/2024_10_30_192956_create_popular_videos_table.php
+++ database/migrations/2024_10_30_192956_create_popular_videos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('popular_video', function (Blueprint $table) {
+        Schema::create('popular_videos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('url');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('popular_video');
+        Schema::dropIfExists('popular_videos');
     }
 };
